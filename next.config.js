02a92ab08a0974b170dc99cf1dd8224060dd8431
maribelcuales <maribelcuales@gmail.com>
2022-04-08 08:24:25 +0800

    Configure apod.nasa.gov for images domain.

--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ["apod.nasa.gov", "https://apod.nasa.gov/", "https://apod.nasa.gov/apod/image/2204/M24_APOD_GabrielRodriguesSantosAPOD1100.jpg" ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  }
+}
