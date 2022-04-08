82ab1b694ba83367383327f8e32039bdcad14e47
maribelcuales <maribelcuales@gmail.com>
2022-04-08 21:33:53 +0800

    Added classnames to elements in photoCard component.

--- components/photoCard.js
+++ components/photoCard.js
@@ -3,17 +3,27 @@ import styles from "../styles/Home.module.css";
 
 export default function PhotoCard(props) {
   return (
-    <div>
-      <img
-        className="nasa-photo-of-the-day"
-        src={props.nasaPhoto.url}
-        alt="Nasa Photo of the Day"
-      />
-      <h2>{props.nasaPhoto.title}</h2>
-      <p>Image Credit and Copyright: {props.nasaPhoto.copyright}</p>
-      <p>{props.nasaPhoto.explanation}</p>
-      <p>Date: {props.nasaPhoto.date}</p>
-      <p>Content and Image Credit: NASA</p>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.photoContainer}>
+        <Image
+          className="nasa-photo"
+          src={props.nasaPhoto.url}
+          alt="Nasa Photo of the Day"
+          height={600}
+          width={800}
+        />
+      </div>
+      <div className={styles.photoDetails}>
+        <h2 className={styles.photoTitle}>{props.nasaPhoto.title}</h2>
+        <p className={styles.copyright}>
+          Image Credit and Copyright: {props.nasaPhoto.copyright}
+        </p>
+        <p className={styles.photoDesc}>{props.nasaPhoto.explanation}</p>
+        <div>
+          <p className={styles.date}>Date: {props.nasaPhoto.date}</p>
+          <p>Content and Image Credit: NASA</p>
+        </div>
+      </div>
+    </main>
   );
 }
