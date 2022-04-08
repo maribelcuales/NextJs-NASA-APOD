5c1ed78e784a6fc4ba015a6a18fbd55bba5e2da1
maribelcuales <maribelcuales@gmail.com>
2022-04-08 08:14:16 +0800

    Added getServerSideProps function to fetch data from NASA APOD API.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import PhotoCard from "../components/photoCard";
 
-export default function Home() {
+export default function Home({ nasaPhoto }) {
+  console.log({ nasaPhoto });
   return (
     <div className={styles.container}>
       <Head>
@@ -13,51 +15,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image 
+        <Image
           className="nasa-logo"
           src="/images/nasa-logo.png"
           alt="Nasa Logo"
           height={144}
-          width={280} 
+          width={280}
         />
-        <h1 className={styles.title}>
-          NASA Photo of the Day
-        </h1>
+        <h1 className={styles.title}>Astronomy Photo of the Day</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <PhotoCard nasaPhoto={nasaPhoto} />
       </main>
 
       <footer className={styles.footer}>
@@ -66,12 +33,22 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=ZpebsFfzozubPgYXkFi5TckqXCf0JsEoofTxqzoZ"
+  );
+  const nasaPhoto = await res.json();
+  return {
+    props: { nasaPhoto },
+  };
 }
